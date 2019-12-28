17f983c1e27ef852949b0c31f83efa5b905e5080
Michael Dono <michael@michaeldono.com>
2019-12-28 22:23:35 +0000

    Refactored carousel into a component

---
+++ src/components/carousel.js
@@ -0,0 +1,23 @@
+import React from "react";
+import BootstrapCarousel from 'react-bootstrap/Carousel'
+
+import styles from './carousel.module.css'
+
+export default ({ items }) => {
+    const carouselItems = items.map( (item) => (
+        <BootstrapCarousel.Item
+            className={styles.carouselImage} 
+            style={ {backgroundImage: `url('${item.imageUrl}')` } }>
+                <div className={styles.carouselContent}>
+                    <div className={styles.caption}>
+                        {item.text}
+                    </div>
+                </div>
+        </BootstrapCarousel.Item>
+    ));
+    return (
+        <BootstrapCarousel className={styles.container}>
+            {carouselItems}
+        </BootstrapCarousel>
+    )
+}
