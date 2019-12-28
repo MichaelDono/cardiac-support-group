8cc727e252354db4e58c8dd9446a3a878681051f
Michael Dono <michael@michaeldono.com>
2019-12-28 20:30:37 +0000

    Added carouselSlide

--- src/templates/index.js
+++ src/templates/index.js
@@ -5,23 +5,15 @@ import { Helmet } from "react-helmet"
 // import Row from 'react-bootstrap/Row'
 // import Col from 'react-bootstrap/Col'
 import Carousel from 'react-bootstrap/Carousel'
-// import CarouselSplash from '../components/carouselSplash'
 import Card from 'react-bootstrap/Card'
 import CardGroup from 'react-bootstrap/CardGroup'
 import NavBar from '../components/navbar'
 import Footer from '../components/footer'
+import CarouselSlide from '../components/carouselSlide'
 import styles from '../pages/index.module.css'
 
 import '../bootstrap/css/bootstrap.css';
 import '../components/fonts.css'
-
-const Content = ({ children }) => (
-    <div className={styles.carouselContent}>
-      <div className={styles.caption}>
-        {children}
-      </div>
-    </div>
-  )
 
 export default ({ data }) => {
     //const content = data.markdownRemark;
@@ -40,33 +32,15 @@ export default ({ data }) => {
       </Helmet>
       <NavBar className={styles.navContainer + " bg-light text-dark"} />
       <Carousel className={styles.content}>
-        <Carousel.Item className={styles.carouselImage} style={
-            {backgroundImage: `url(img/chair-exercises-for-seniors.jpg)`,
-            backgroundPosition: `center`,
-            backgroundSize: `cover`}
-            } >
-              <Content>
-                <p>Take part in our fun-filled exercise classes run by our fully qualified instructors.</p>
-              </Content>
-        </Carousel.Item>
-        <Carousel.Item className={styles.carouselImage} style={
-            {backgroundImage: `url(img/walking.jpeg)`,
-            backgroundPosition: `center`,
-            backgroundSize: `cover`}
-            } >
-              <Content>
-                <p>Join one of our affordable summer day trips.</p>
-              </Content>
-        </Carousel.Item>
-        <Carousel.Item className={styles.carouselImage} style={
-            {backgroundImage: `url(img/cafe.jpeg)`,
-            backgroundPosition: `center`,
-            backgroundSize: `cover`}
-            } >
-              <Content>
-                <p>Come along to our monthly relaxed and informal social evenings.</p>
-              </Content>
-        </Carousel.Item>
+          <CarouselSlide imageUrl="img/chair-exercises-for-seniors.jpg">
+            <p>Take part in our fun-filled exercise classes run by our fully qualified instructors.</p>
+          </CarouselSlide>
+          <CarouselSlide imageUrl="img/walking.jpeg">
+            <p>Join one of our affordable summer day trips.</p>
+          </CarouselSlide>
+          <CarouselSlide imageUrl="img/cafe.jpeg">
+          <p>Come along to our monthly relaxed and informal social evenings.</p>
+          </CarouselSlide>
       </Carousel>
       <div className={styles.lower}>
       <CardGroup className={styles.card}>
