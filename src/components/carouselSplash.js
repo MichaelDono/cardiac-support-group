09f0412724fbe62ad9df3fa2ee41ae1b7aad7679
Michael Dono <michael@michaeldono.com>
2019-12-28 22:24:46 +0000

    Deleted old index page

--- src/components/carouselSplash.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Carousel from 'react-bootstrap/Carousel'
-
-export default (props) => (
-    <Carousel.Item style={
-        {backgroundImage: props.image}
-        } >
-    </Carousel.Item>
-)
