2b7e4c7672bd88167f6c02f13ee49cd3f6585c90
Michael Dono <michael@michaeldono.com>
2020-01-18 17:30:33 +0000

    Fixed issue with gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,7 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, actions }) => {
     const { createNodeField } = actions
     if (node.internal.type === `MarkdownRemark`) {
+        console.log(createFilePath({ node, getNode }))
         const value = createFilePath({ node, getNode, basePath: `pages` })
         createNodeField({
         name: `slug`,
