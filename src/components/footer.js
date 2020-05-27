413007bb304495834c815ddd8a414c5e28cb910b
Michael Dono <michael@michaeldono.com>
2020-05-27 18:35:28 +0100

    Prototype of new footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,48 @@
 import React from "react"
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-// import styles from './footer.module.css'
+import styles from './footer.module.css'
 
 export default (props) => (
-<Row className={props.className}>
-    <Col lg="2"/>
-    <Col>
-        <ul className="nav justify-content-center nav-fill">
-            <li class="nav-item">
-                <p class="nav-link">© Sunderland Cardiac Support Group</p>
-            </li>
-            <li class="nav-item">
-                <p class="nav-link">Registered Charity Number 1126524</p>
-            </li>
-        </ul>
-    </Col>
-    <Col lg="2" />
-</Row>)
+<div className={styles.container}>
+    <div className={styles.content}>
+        <div className={styles.contact}>
+            <h1 className={styles.header}>Contact Us</h1>
+            <div>
+                <span>phone</span>
+                <span>0191 5656892</span>
+            </div>
+            <div>
+                <span>email</span>
+                <span>hello@sunderlandcardiac.org.uk</span>
+            </div>
+        </div>
+        <div className={styles.quickLinks}>
+            <h1 className={styles.header}>Quick Links</h1>
+            <div>
+                <div>
+                    <a>Info &amp; Support</a>
+                    <a>About Us</a>
+                </div>
+                <div>
+                    <a>How to Join</a>
+                    <a>News &amp; Events</a>
+                </div>
+                
+            </div>
+        </div>
+        <div className={styles.legalLinks}>
+            <a>Your Privacy</a>
+            <a>Cookies</a>
+        </div>
+        <div className={styles.logo}>
+            <img src="img/header_white.svg"
+                    height="42"
+                    alt="Sunderland Cardiac Support Group Logo"/>
+        </div>
+        <div className={styles.legal}>
+            <p>© 2020 Sunderland Cardiac Support Group.</p>
+            <p>Registered charity in England and Wales (1126524).</p>
+        </div>
+    </div>
+</div>)
