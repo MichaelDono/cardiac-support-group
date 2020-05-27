c211e45f169b52140e7bcd891a60c13d3b4b784e
Michael Dono <michael@michaeldono.com>
2020-05-27 20:31:48 +0100

    Added links to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,36 +10,33 @@ export default (props) => (
             <h1 className={styles.header}>Contact Us</h1>
             <div>
                 <i className="material-icons">phone</i>
-                <span>0191 5656892</span>
+                <span className={styles.contactMethod}>0191 5656892</span>
             </div>
             <div>
                 <i className="material-icons">mail_outline</i>
-                <span>hello@sunderlandcardiac.org.uk</span>
+                <a href="mailto: hello@sunderlandcardiac.org.uk" className={styles.contactMethod}>hello@sunderlandcardiac.org.uk</a>
             </div>
         </div>
         <div className={styles.quickLinks}>
             <h1 className={styles.header}>Quick Links</h1>
             <div>
-                <div>
-                    <a>Info &amp; Support</a>
-                    <a>About Us</a>
-                </div>
-                <div>
-                    <a>How to Join</a>
-                    <a>News &amp; Events</a>
-                </div>
-                
+                <a>Info &amp; Support</a>
+                <a>About Us</a>
+            </div>
+            <div>
+                <a>How to Join</a>
+                <a>News &amp; Events</a>
             </div>
         </div>
         <div className={styles.legalLinks}>
             <a>Your Privacy</a>
             <a>Cookies</a>
         </div>
-        <div className={styles.logo}>
+        <a className={styles.logo} href="/">
             <img src="img/header_white.svg"
                     height="42"
                     alt="Sunderland Cardiac Support Group Logo"/>
-        </div>
+        </a>
         <div className={styles.legal}>
             <p>© 2020 Sunderland Cardiac Support Group.</p>
             <p>Registered charity in England and Wales (1126524).</p>
