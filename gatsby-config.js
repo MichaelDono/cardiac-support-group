25c625e38a5c7e2e50fdb2250b0b2faa236db26d
Michael Dono <michael@michaeldono.com>
2020-06-02 11:14:59 +0100

    Footer now updates per config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,8 @@ module.exports = {
     title: `Sunderland Cardiac Support Group`,
     description: `We are a registered charity run by volunteers dedicated to providing help and support to the people of Sunderland and the surrounding districts who have or have suffered from some form of cardiac trauma. We provide exercise classes, walks, social evenings and much more.`,
     author: `Michael Dono`,
+    phone: `0191 5656892`,
+    email: `hello@sunderlandcardiac.org.uk`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -16,12 +18,5 @@ module.exports = {
         name: 'pages',
       },
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/data`,
-        name: 'data',
-      },
-    }
   ],
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,43 +1,59 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import styles from './footer.module.css'
 
-export default (props) => (
-<div className={styles.container}>
-    <div className={styles.content}>
-        <div className={styles.contact}>
-            <h1 className={styles.header}>Contact Us</h1>
-            <div>
-                <i className="material-icons">phone</i>
-                <span className={styles.contactMethod}>0191 5656892</span>
+export default () => {
+    const { site } = useStaticQuery(
+        graphql`
+            query {
+                site {
+                siteMetadata {
+                    phone
+                    email
+                }
+                }
+            }
+        `
+    )
+    return (
+    <div className={styles.container}>
+        <div className={styles.content}>
+            <div className={styles.contact}>
+                <h1 className={styles.header}>Contact Us</h1>
+                <div>
+                    <i className="material-icons">phone</i>
+                    <span className={styles.contactMethod}>{site.siteMetadata.phone}</span>
+                </div>
+                <div>
+                    <i className="material-icons">mail_outline</i>
+                    <a href={"mailto: " + site.siteMetadata.email} className={styles.contactMethod}>{site.siteMetadata.email}</a>
+                </div>
             </div>
-            <div>
-                <i className="material-icons">mail_outline</i>
-                <a href="mailto: hello@sunderlandcardiac.org.uk" className={styles.contactMethod}>hello@sunderlandcardiac.org.uk</a>
+            <div className={styles.quickLinks}>
+                <h1 className={styles.header}>Quick Links</h1>
+                <div>
+                    <a>Info &amp; Support</a>
+                    <a>About Us</a>
+                </div>
+                <div>
+                    <a>How to Join</a>
+                    <a>News &amp; Events</a>
+                </div>
             </div>
-        </div>
-        <div className={styles.quickLinks}>
-            <h1 className={styles.header}>Quick Links</h1>
-            <div>
-                <a>Info &amp; Support</a>
-                <a>About Us</a>
+            <div className={styles.legalLinks}>
+                <a>Your Privacy</a>
+                <a>Cookies</a>
             </div>
-            <div>
-                <a>How to Join</a>
-                <a>News &amp; Events</a>
+            <a className={styles.logo} href="/">
+                <img src="img/header_white.svg"
+                        height="42"
+                        alt="Sunderland Cardiac Support Group Logo"/>
+            </a>
+            <div className={styles.legal}>
+                <p>© 2020 Sunderland Cardiac Support Group.</p>
+                <p>Registered charity in England and Wales (1126524).</p>
             </div>
-        </div>
-        <div className={styles.legalLinks}>
-            <a>Your Privacy</a>
-            <a>Cookies</a>
-        </div>
-        <a className={styles.logo} href="/">
-            <img src="img/header_white.svg"
-                    height="42"
-                    alt="Sunderland Cardiac Support Group Logo"/>
-        </a>
-        <div className={styles.legal}>
-            <p>© 2020 Sunderland Cardiac Support Group.</p>
-            <p>Registered charity in England and Wales (1126524).</p>
         </div>
     </div>
-</div>)
+    )
+}
