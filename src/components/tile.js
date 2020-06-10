44c36ff040a8d7e8f91cf650b6ae17332e3454a6
Michael Dono <michael@michaeldono.com>
2020-06-10 21:59:04 +0100

    Added initial tile component for homepage

---
+++ src/components/tile.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+import styles from './tile.module.css'
+
+export default () => { 
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                
+                
+            </div>
+            
+        </div>
+    )
+
+}
