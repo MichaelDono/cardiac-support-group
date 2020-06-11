cfbc5a5e3f688aecfa3afd87af6f4ad2ccded7e1
Michael Dono <michael@michaeldono.com>
2020-06-11 13:28:29 +0100

    Implemented a bodge on title text 🙃

--- src/components/titleText.js
+++ src/components/titleText.js
@@ -3,8 +3,14 @@ import styles from './titleText.module.css'
 
 export default (props) => { 
     return (
+    
+    <> 
+    <a href="/" className={styles.hidden}>
+        <span>{props.children}</span>
+    </a>
     <a href="/" className={styles.container}>
         <span>{props.children}</span>
     </a>
+    </>
     );
 }
