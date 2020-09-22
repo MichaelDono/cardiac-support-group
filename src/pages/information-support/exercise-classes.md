5715292240ed0b44d9ae4d5d8761684ba2b12cce
Michael Dono <michael@michaeldono.com>
2020-09-22 13:22:48 +0100

    Added exercise classes content to cms

--- src/pages/information-support/exercise-classes.md
+++ src/pages/information-support/exercise-classes.md
@@ -1,5 +1,27 @@
 ---
 title: Welcome
 templateKey: exercise-classes
+times:
+  "Monday": ["10:00 - 11:00", "11:15 - 12:15"]
+  "Tuesday": [""]
+  "Wednesday": ["10:30 - 11:30"]
+  "Thursday": ["10:00 - 11:00", "11:15 - 12:15"]
+  "Friday": [""]
+  "Saturday": [""]
+  "Sunday": [""]
+featured:
+  image:
+    url: "../../data/img/Seniors_exercising.jpeg"
+    alt: Cardiac patients exercising as a group
+  body: |
+    Following completion of the Steps to Health programme...
+main:
+  - heading: How does cardiac rehab help?
+    image:
+      url: ""
+      alt: ""
+    body: |
+      We offer exercise classes, education sessions as well as relaxational and recreational activities.
+      We aim to keep this all very affordable, at little to no cost to you.
 isPage: true
 ---
--- src/templates/exercise-classes.js
+++ src/templates/exercise-classes.js
@@ -4,7 +4,8 @@ import SEO from '../components/seo'
 import Footer from '../components/footer'
 import Navbar from '../components/navbar'
 import Breadcrumbs from '../components/breadcrumbs'
-import styles from './information-support.module.css'
+import Img from "gatsby-image/withIEPolyfill"
+import styles from './exercise-classes.module.css'
 
 import '../bootstrap/css/bootstrap.css';
 import '../components/fonts.css'
@@ -17,18 +18,105 @@ export default ({ pageContext, data }) => {
     <Navbar />
     <div className={styles.content}>
       <Breadcrumbs crumbs={crumbs} />
+      <Header content={data.page.frontmatter.featured} />
+      <MainContent content={data.page.frontmatter.main} />
+      <div className={styles.main}>
+        <div>
+          <table className={"table table-bordered my-3"}>
+            <thead>
+              <th>Day</th>
+              <th>Times</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td rowspan="2">Monday</td>
+                <td>10:00 - 11:00</td>
+              </tr>
+              <tr>
+                <td>11:15 - 12:15</td>
+              </tr>
+              <tr>
+                <td>Wednesday</td>
+                <td>10:30 - 11:30</td>
+              </tr>
+              <tr>
+                <td rowspan="2">Thursday</td>
+                <td>10:00 - 11:00</td>
+              </tr>
+              <tr>
+                <td>11:15 - 12:15</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
     <Footer className={styles.footerContainer} />
   </div>
   )
 }
 
+let Header = ({content}) => {
+  return (
+    <div className={styles.header}>
+      <div>
+        <h1>Exercise Classes</h1>
+        <p>{content.body}</p>
+        <Img fluid={content.image.url.childImageSharp.fluid} className={styles.headerImage} objectFit="cover" objectPosition="50% 10%"></Img>
+      </div>
+    </div>
+    )
+}
+
+let MainContent = ({content}) => {
+  return (
+    <div className={styles.mainContent}>
+      {content.map( entry => (
+        <div>
+          <h2>{entry.heading}</h2>
+          <p>{entry.body}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
 export const query = graphql`
-query {
+query($slug: String!) {
   site {
     siteMetadata {
       description
       title
+    }
+  }
+  page: markdownRemark(fields: { slug: { eq: $slug } }) {
+    frontmatter {
+      times {
+        Monday
+        Tuesday
+        Wednesday
+        Thursday
+        Friday
+        Saturday
+        Sunday
+      }
+      featured {
+        body
+        image {
+          alt
+          url {
+            childImageSharp {
+              fluid(maxWidth: 750) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+      main {
+        heading
+        body
+      }
     }
   }
 }
